671ded3fdf6dfecb8e0fa773ff8646ab163047de
Christian López <christian.lopez4@u.icesi.edu.co>
2021-08-08 15:50:57 -0500

    Termino de cubrir las pruebas

--- src/app/feature/paciente/components/crear-paciente/crear-paciente.component.spec.ts
+++ src/app/feature/paciente/components/crear-paciente/crear-paciente.component.spec.ts
@@ -12,11 +12,26 @@ import { of } from 'rxjs';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CategoriaService } from '@shared/services/categoria/service/categoria.service';
 import { DocumentoService } from '@shared/services/documento/service/documento.service';
+import { Categoria } from '@shared/services/categoria/model/categoria';
+import { Documento } from '@shared/services/documento/model/documento';
 
 describe('CrearPacienteComponent', () => {
   let component: CrearPacienteComponent;
   let fixture: ComponentFixture<CrearPacienteComponent>;
   let pacienteService: PacienteService;
+  let documentoService: DocumentoService;
+  let categoriaService: CategoriaService;
+  const categorias: Categoria[] = [
+    new Categoria(1, 'A', 3000, 5000),
+    new Categoria(2, 'B', 5000, 8000),
+    new Categoria(3, 'C', 8000, 12000)
+  ];
+  const documentos: Documento[] = [
+    new Documento(1, 'Doc1'),
+    new Documento(2, 'Doc2'),
+    new Documento(3, 'Doc3')
+  ];
+
   const valorEsperado = 123;
 
   beforeEach(waitForAsync(() => {
@@ -38,9 +53,17 @@ describe('CrearPacienteComponent', () => {
     fixture = TestBed.createComponent(CrearPacienteComponent);
     component = fixture.componentInstance;
     pacienteService = TestBed.inject(PacienteService);
+    documentoService = TestBed.inject(DocumentoService);
+    categoriaService = TestBed.inject(CategoriaService);
 
     spyOn(pacienteService, 'guardar').and.returnValue(
       of(valorEsperado)
+    );
+    spyOn(documentoService, 'consultar').and.returnValue(
+      of(documentos)
+    );
+    spyOn(categoriaService, 'consultar').and.returnValue(
+      of(categorias)
     );
     fixture.detectChanges();
   });
@@ -62,6 +85,16 @@ describe('CrearPacienteComponent', () => {
     document.getElementById('submit').click();
 
     expect(component.idPaciente).toBe(123);
+  });
+
+  it('debería listar categorias', () => {
+    expect(component.categorias.length).toBe(3);
+    expect(component.categorias).toEqual(categorias);
+  });
+
+  it('debería listar documentos', () => {
+    expect(component.documentos.length).toBe(3);
+    expect(component.documentos).toEqual(documentos);
   });
 
 });
