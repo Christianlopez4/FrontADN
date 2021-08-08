b26e16247b9c20f6b752f20c8cb599bf423a03a5
Christian López <christian.lopez4@u.icesi.edu.co>
2021-08-08 16:06:43 -0500

    x

--- src/app/shared/services/categoria/service/categoria.service.spec.ts
+++ src/app/shared/services/categoria/service/categoria.service.spec.ts
@@ -1,19 +1,45 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { HttpService } from '@core/services/http.service';
+import { environment } from 'src/environments/environment';
+import { Categoria } from '../model/categoria';
 
 import { CategoriaService } from './categoria.service';
 
 describe('CategoriaService', () => {
-  let httpServiceSpy: { get: jasmine.Spy };
-  let categoriaService: CategoriaService;
+  let httpMock: HttpTestingController;
+  let service: CategoriaService;
+  const apiEndpointCategoriasConsulta = `${environment.endpoint}/categorias`;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    httpServiceSpy = jasmine.createSpyObj('HttpService', ['doGet']);
-    categoriaService = new CategoriaService(httpServiceSpy as any);
+    const injector = TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [CategoriaService, HttpService]
+    });
+    httpMock = injector.inject(HttpTestingController);
+    service = TestBed.inject(CategoriaService);
   });
 
   it('should be created', () => {
-    expect(categoriaService).toBeTruthy();
+    expect(service).toBeTruthy();
+  });
+
+  it('debería listar las categorias', () => {
+    const dummyCategorias: Categoria[] = [
+      new Categoria(1, 'A', 3000, 5000),
+      new Categoria(2, 'B', 5000, 8000),
+      new Categoria(3, 'C', 8000, 12000)
+    ];
+
+    service.consultar().subscribe(respuesta => {
+      expect(respuesta.length).toBe(3);
+      expect(respuesta).toEqual(dummyCategorias);
+    });
+
+    const req = httpMock.expectOne(apiEndpointCategoriasConsulta);
+    expect(req.request.method).toBe('GET');
+    req.flush(dummyCategorias);
+
   });
 
 });
